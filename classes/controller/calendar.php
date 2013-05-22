f2ade33b2e7c8e4ab6e2933ac8ee855623a64960
Shintaro Ikezaki <ikezaki@impv.co.jp>
2013-05-22 11:19:14 +0900

    controller allows accessing via HMVC

--- classes/controller/calendar.php
+++ classes/controller/calendar.php
@@ -4,14 +4,25 @@ namespace Calendar;
 
 class Controller_Calendar extends \Controller
 {
+	public function before()
+	{
+		if ( ! \Request::is_hmvc())
+		{
+			// This controller allows accessing via HMVC.
+			throw new \HttpNotFoundException;
+		}
+	}
+
 	public function action_year($year = null)
 	{
 		return \Response::forge(\View::forge('calendar/year', array('year' => Calendar::year($year))));
 	}
+
 	public function action_month($year = null, $month = null)
 	{
 		return \Response::forge(\View::forge('calendar/month', array('month' => Calendar::month($month, $year))));
 	}
+	
 	public function action_week($year = null, $month = null, $week = null)
 	{
 		return \Response::forge(\View::forge('calendar/week', array('week' => Calendar::week($week, $month, $year))));
